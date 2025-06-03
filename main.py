5df0174ca6d317656caeb6ec528b1dacf57a55fe
Test <kilianberteaudca@gmail.com>
2025-06-03 13:38:14 -0400

    Ajout main

---
+++ main.py
@@ -0,0 +1,10 @@
+"""---------------Mini-Projet B - Intégration Numérique----------------------
+
+    - Kilian Berteaud
+    - Enzo Villamandos
+    - Lucas Bonneaud
+
+Cours MGA802, Session Été 2025
+"""
+
+#PROGRAMME PRINCIPAL
