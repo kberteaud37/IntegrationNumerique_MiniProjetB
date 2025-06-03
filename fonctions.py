0ce23619da791bffb0ba097a728c5869807f0b23
enzo <enzzovillamandos@gmail.com>
2025-06-03 14:43:30 -0400

    fonction trapeze

--- fonctions.py
+++ fonctions.py
@@ -1,2 +1,26 @@
 def f(a,b,c,d,x):
     return a+b*x+c*x**2+d*x**3
+p1 = 3
+p2 = 2
+p3 = 4
+a = -25
+b=25
+n = 100
+pas = abs(a)+abs()
+
+
+fonction_polynomiale = p1+p2*x+p3*(x**2)+p4*(x**3)
+
+def python_trapeze (a,b,fonction_polynomiale):
+p1 = 3
+p2 = 2
+p3 = 4
+a = -25
+b=25
+n = 100
+pas = abs(a)+abs()
+
+
+fonction_polynomiale = p1+p2*x+p3*(x**2)+p4*(x**3)
+
+def python_trapeze (a,b,fonction_polynomiale):
