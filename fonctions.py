4d4cbd726867afce1084965575123e696f20f8e1
bonne <bonneaudlucass@gmail.com>
2025-06-03 15:26:32 -0400

    prototype de la fonction rectangle

--- fonctions.py
+++ fonctions.py
@@ -6,20 +6,26 @@ def integration_rectangle () :
     p3 = float(input('veuillez entrer la constante p3 : '))
     p4 = float(input('veuillez entrer la constante p4 : '))
 
-    # borne_inferieur = input('veuillez entrer la borne inferieur : ')
-    # borne_superieur = input('veuillez entrer la borne superieur : ')
+    air = 0
 
-    x_vals = [-100000]
-    y_vals = [100000]
-
-    n = 10
-    for i in range (-25,25,n):
+    n = 1
+    for i in range (-10,11,n):
         equation = ((p1) +
                     (p2) *(i) +
                     (p3) * (i)**2 +
                     (p4)  *(i)**3)
 
-    plt.plot(x_vals, y_vals, marker='o', linestyle='-')
+
+        plt.plot([i-(n/2), i+(n/2)], [equation, equation], color='red', linewidth=2, label='Mon segment')
+        plt.plot([i-(n/2), i-(n/2)], [equation, 0], color='red', linewidth=2, label='Mon segment')
+        plt.plot([i+(n/2), i+(n/2)], [equation, 0], color='red', linewidth=2, label='Mon segment')
+
+        air += (n * (equation))
+
+        plt.plot(i, equation, marker='o', linestyle='-')
+
+    print(air)
+
     plt.title("Tracé de l'équation polynomiale")
     plt.xlabel("i")
     plt.ylabel("Valeur de l'équation")
@@ -30,4 +36,5 @@ def integration_rectangle () :
 integration_rectangle()
 
 
-print
+# Tracer le segment
+
